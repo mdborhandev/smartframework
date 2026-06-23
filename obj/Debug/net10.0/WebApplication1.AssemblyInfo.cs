e39a3c699e2d3001da90b8533fc555b419fa75e8
mdborhandev <mdborhan.dev@gmail.com>
2026-06-23 12:05:49 +0600

    sd

--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aebecc90447bbae859e43d71742c114b147e0a30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004f83aa9fb1f08fef017e5587219219a719059f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
